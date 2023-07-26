a269679cbbffece8b794679fd0689389048c85e8
stefanspringer1 <stefan.springer@icloud.com>
2023-07-26 11:54:07 +0200

    dispense with SwiftyRelativePath

--- Package.swift
+++ Package.swift
@@ -17,7 +17,6 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.1"),
-        .package(url: "https://github.com/stefanspringer1/SwiftyRelativePath", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,7 +25,6 @@ let package = Package(
             name: "Utilities",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                "SwiftyRelativePath",
             ]
         ),
         .testTarget(
