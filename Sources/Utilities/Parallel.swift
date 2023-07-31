bb164be2853e3d0d7d706e1399aa25eba90c9a05
stefanspringer1 <stefan.springer@icloud.com>
2023-07-31 12:46:20 +0200

    added utilities for parallel execution

--- Sources/Utilities/Parallel.swift
+++ Sources/Utilities/Parallel.swift
@@ -5,7 +5,7 @@
 import Foundation
 
 /// Process the items in `batch` in parallel by the function `worker` using `threads` number of threads.
-public func executeInParallel<Seq: Sequence>(batch: Seq, worker: @escaping (Seq.Element) -> (), threads: Int) {
+public func executeInParallel<Seq: Sequence>(batch: Seq, threads: Int, worker: @escaping (Seq.Element) -> ()) {
     let queue = DispatchQueue(label: "AyncLogger", attributes: .concurrent)
     let group = DispatchGroup()
     let semaphore = DispatchSemaphore(value: threads)
