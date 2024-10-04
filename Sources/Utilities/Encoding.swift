c57953b1319763562d516c6b87a1b2b21033173d
stefanspringer1 <stefan.springer@icloud.com>
2024-10-04 15:26:17 +0200

    new: EncodingArgument

--- Sources/Utilities/Encoding.swift
+++ Sources/Utilities/Encoding.swift
@@ -1,30 +1,34 @@
 import Foundation
+import ArgumentParser
 
-func encoding(fromName name:  String) -> String.Encoding? {
-    switch name {
-    case "ascii":  .ascii
-    case "iso2022JP":  .iso2022JP
-    case "isoLatin1":  .isoLatin1
-    case "isoLatin2":  .isoLatin2
-    case "japaneseEUC":  .japaneseEUC
-    case "macOSRoman":  .macOSRoman
-    case "nextstep":  .nextstep
-    case "nonLossyASCII":  .nonLossyASCII
-    case "shiftJIS":  .shiftJIS
-    case "symbol":  .symbol
-    case "unicode":  .unicode
-    case "utf16":  .utf16
-    case "utf16BigEndian":  .utf16BigEndian
-    case "utf16LittleEndian":  .utf16LittleEndian
-    case "utf32":  .utf32
-    case "utf32BigEndian":  .utf32BigEndian
-    case "utf32LittleEndian":  .utf32LittleEndian
-    case "utf8":  .utf8
-    case "windowsCP1250":  .windowsCP1250
-    case "windowsCP1251":  .windowsCP1251
-    case "windowsCP1252":  .windowsCP1252
-    case "windowsCP1253":  .windowsCP1253
-    case "windowsCP1254":  .windowsCP1254
-    default:  nil
+public enum EncodingArgument: String, ExpressibleByArgument {
+    case ascii, iso2022JP, isoLatin1, isoLatin2, japaneseEUC, macOSRoman, nextstep, nonLossyASCII, shiftJIS, symbol, unicode, utf16, utf16BigEndian, utf16LittleEndian, utf32, utf32BigEndian, utf32LittleEndian, utf8, windowsCP1250, windowsCP1251, windowsCP1252, windowsCP1253, windowsCP1254
+    
+    var encoding: String.Encoding {
+        switch self {
+        case .ascii: .ascii
+        case .iso2022JP: .iso2022JP
+        case .isoLatin1: .isoLatin1
+        case .isoLatin2: .isoLatin2
+        case .japaneseEUC: .japaneseEUC
+        case .macOSRoman: .macOSRoman
+        case .nextstep: .nextstep
+        case .nonLossyASCII: .nonLossyASCII
+        case .shiftJIS: .shiftJIS
+        case .symbol: .symbol
+        case .unicode: .unicode
+        case .utf16: .utf16
+        case .utf16BigEndian: .utf16BigEndian
+        case .utf16LittleEndian: .utf16LittleEndian
+        case .utf32: .utf32
+        case .utf32BigEndian: .utf32BigEndian
+        case .utf32LittleEndian: .utf32LittleEndian
+        case .utf8: .utf8
+        case .windowsCP1250: .windowsCP1250
+        case .windowsCP1251: .windowsCP1251
+        case .windowsCP1252: .windowsCP1252
+        case .windowsCP1253: .windowsCP1253
+        case .windowsCP1254: .windowsCP1254
+        }
     }
 }
