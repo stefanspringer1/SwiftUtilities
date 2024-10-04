4cc57fff3f3f2941908adc7f266fa8ce77a9c8f4
stefanspringer1 <stefan.springer@icloud.com>
2024-10-04 15:30:21 +0200

    made EncodingArgument CaseIterable

--- Sources/Utilities/Encoding.swift
+++ Sources/Utilities/Encoding.swift
@@ -1,7 +1,7 @@
 import Foundation
 import ArgumentParser
 
-public enum EncodingArgument: String, ExpressibleByArgument {
+public enum EncodingArgument: String, ExpressibleByArgument, CaseIterable {
     case ascii, iso2022JP, isoLatin1, isoLatin2, japaneseEUC, macOSRoman, nextstep, nonLossyASCII, shiftJIS, symbol, unicode, utf16, utf16BigEndian, utf16LittleEndian, utf32, utf32BigEndian, utf32LittleEndian, utf8, windowsCP1250, windowsCP1251, windowsCP1252, windowsCP1253, windowsCP1254
     
     var encoding: String.Encoding {
