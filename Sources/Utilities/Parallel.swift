0193c26c798771878d660ba21bb4c106100fff51
stefanspringer1 <stefan.springer@icloud.com>
2026-01-14 11:57:39 +0100

    parallel: maximalSimultaneousOperations might be nil

--- Sources/Utilities/Parallel.swift
+++ Sources/Utilities/Parallel.swift
@@ -19,7 +19,8 @@ public func parallel<T: Sendable>(batch: Array<T>, worker: @escaping @Sendable (
 /// Process the items in `batch` in parallel by the function `worker`,
 /// but no more than`maximalSimultaneousOperations` at the same time
 /// (the number of items actually being processed simultaneously could be lower).
-public func parallel<T: Sendable>(batch: Array<T>, maximalSimultaneousOperations: Int, worker: @escaping @Sendable (T) async -> ()) async {
+public func parallel<T: Sendable>(batch: Array<T>, maximalSimultaneousOperations: Int?, worker: @escaping @Sendable (T) async -> ()) async {
+    guard let maximalSimultaneousOperations else { return await parallel(batch: batch, worker: worker) }
     await withTaskGroup(of: Void.self) { taskGroup in
         for (index,work) in batch.enumerated() {
             if index >= maximalSimultaneousOperations {
